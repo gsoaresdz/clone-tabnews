9f31a2be684189517e46aa6c148ee46695425b31
Gabriel Soares <69989654+gsoaresdz@users.noreply.github.com>
2023-09-06 11:45:39 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>“Para ter sabedoria, é preciso primeiro pagar o seu preço. Use tudo o que você tem para adquirir o entendimento. Provérbios 4:7”</h1>
+}
+
+export default Home;
