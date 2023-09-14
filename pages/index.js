65378a8cc7a3c468bff36246c934ca5aedb3a33c
Gabriel Soares <69989654+gsoaresdz@users.noreply.github.com>
2023-09-14 12:36:52 +0000

    adiciona scripts line:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>“Para ter sabedoria, é preciso primeiro pagar o seu preço. Use tudo o que você tem para adquirir o entendimento. Provérbios 4:7” 📖</h1>
+  return (
+    <h1>
+      “Para ter sabedoria, é preciso primeiro pagar o seu preço. Use tudo o que
+      você tem para adquirir o entendimento. Provérbios 4:7” 📖
+    </h1>
+  );
 }
 
 export default Home;
